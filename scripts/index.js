307c7564e5942f9237550ebe9a1c15e3ccb48ce2
Cesar <cesarfranchi111@gmail.com>
2024-03-07 22:34:40 -0300

    Validador de Vencedor

--- scripts/index.js
+++ scripts/index.js
@@ -16,11 +16,44 @@ function $toogleCurrentMove() {
   }
 }
 
+function verifyFields(firstField, secondField, thirdField) {
+  const $fieldList = document.querySelectorAll(".scenary-field-big");
+  const hasWinner =
+    $fieldList[firstField].textContent != "" &&
+    $fieldList[firstField].textContent == $fieldList[secondField].textContent &&
+    $fieldList[secondField].textContent == $fieldList[thirdField].textContent;
+
+  return hasWinner;
+}
+
+function getWinner() {
+  if (verifyFields(0, 1, 2)) {
+    return game.currentMove;
+  } else if (verifyFields(3, 4, 5)) {
+    return game.currentMove;
+  } else if (verifyFields(6, 7, 8)) {
+    return game.currentMove;
+  } else if (verifyFields(0, 3, 6)) {
+    return game.currentMove;
+  } else if (verifyFields(1, 4, 7)) {
+    return game.currentMove;
+  } else if (verifyFields(2, 5, 8)) {
+    return game.currentMove;
+  } else if (verifyFields(0, 4, 8)) {
+    return game.currentMove;
+  } else if (verifyFields(2, 4, 6)) {
+    return game.currentMove;
+  }
+
+  return "";
+}
+
 for (let i = 0; i < 9; i++) {
   const $field = getField(i);
 
   $field.addEventListener("click", function () {
     $field.textContent = game.currentMove;
-    $toogleCurrentMove()
+    console.log(getWinner());
+    $toogleCurrentMove();
   });
 }
