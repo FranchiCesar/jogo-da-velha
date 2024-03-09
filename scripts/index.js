0fe2117cd85d45a6fc5158acc9ceb5cc176b1b82
Cesar <cesarfranchi111@gmail.com>
2024-03-08 22:36:14 -0300

    Placar

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,10 @@
 const game = {
+  start: true,
   currentMove: "X",
+  players: {
+    score1: 0,
+    score2: 0,
+  },
 };
 
 function getField(fieldNumber) {
@@ -9,9 +14,9 @@ function getField(fieldNumber) {
 }
 
 function $toogleCurrentMove() {
-  if (game.currentMove == "X") {
+  if (game.currentMove === "X") {
     game.currentMove = "O";
-  } else if (game.currentMove == "O") {
+  } else if (game.currentMove === "O") {
     game.currentMove = "X";
   }
 }
@@ -20,8 +25,9 @@ function verifyFields(firstField, secondField, thirdField) {
   const $fieldList = document.querySelectorAll(".scenary-field-big");
   const hasWinner =
     $fieldList[firstField].textContent != "" &&
-    $fieldList[firstField].textContent == $fieldList[secondField].textContent &&
-    $fieldList[secondField].textContent == $fieldList[thirdField].textContent;
+    $fieldList[firstField].textContent ===
+      $fieldList[secondField].textContent &&
+    $fieldList[secondField].textContent === $fieldList[thirdField].textContent;
 
   return hasWinner;
 }
@@ -48,12 +54,68 @@ function getWinner() {
   return "";
 }
 
+function addPlayerScore(winner) {
+  if (winner === "X") {
+    game.players.score1 += 1;
+  } else if (winner === "O") {
+    game.players.score2 += 1;
+  }
+}
+
+function printPlayerScore() {
+  const [$score1, $score2] = document.querySelectorAll(".score");
+
+  $score1.textContent = game.players.score1;
+  $score2.textContent = game.players.score2;
+}
+
+function resetBoard() {
+  const $fieldList = document.querySelectorAll(".scenary-field-big");
+
+  for (const $field of $fieldList) {
+    $field.textContent = "";
+  }
+}
+
+function getPlayerName(move) {
+  if ((move = "X")) {
+    const $inputPlayer1 = document.querySelector(".player-field-1");
+    return $inputPlayer1.value;
+  } else if ((move = "O")) {
+    const $inputPlayer2 = document.querySelector(".player-field-2");
+    return $inputPlayer2.value;
+  }
+}
+
+function printWinnerName(winnerName) {
+  const $winnerField = document.querySelector(".winner-field");
+
+  $winnerField.textContent = winnerName;
+}
+
 for (let i = 0; i < 9; i++) {
   const $field = getField(i);
 
   $field.addEventListener("click", function () {
+    if ($field.textContent != "" || game.start === false) return;
     $field.textContent = game.currentMove;
-    console.log(getWinner());
+
+    const winner = getWinner();
+    if (winner !== "") {
+      addPlayerScore(winner);
+      printPlayerScore();
+      setTimeout(resetBoard, 1000);
+      game.start = false;
+
+      const winnerName = getPlayerName(winner);
+
+      printWinnerName(winnerName);
+      
+      setTimeout(function () {
+        game.start = true;
+      }, 1000);
+    }
+
     $toogleCurrentMove();
   });
 }
